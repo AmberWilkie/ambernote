92a2aef2df33fd5d1cedf8bfdcfef6aaa194ee5a
Amber <amber@amberwilkie.com>
2016-08-27 21:03:36 +0200

    Set up DatabaseCleaner

--- lib/models/user.rb
+++ lib/models/user.rb
@@ -1,12 +1,13 @@
 require 'data_mapper'
 require 'bcrypt'
+require 'pry'
 
 class User
   include DataMapper::Resource
 
   property :id, Serial, key: true
   property :username, String
-  property :password, BCryptHash
+  property :password, String
   # has n, :entries
 
   def authenticate(attempted_password)
