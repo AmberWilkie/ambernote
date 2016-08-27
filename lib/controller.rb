7c03b81f273bf81b1e940139b861c432cf8f0645
Amber <amber@amberwilkie.com>
2016-08-27 15:12:25 +0200

    Bundle and controller requirements

--- lib/controller.rb
+++ lib/controller.rb
@@ -1,9 +1,9 @@
 require 'bundler'
 Bundler.require
 Dir[File.join(File.dirname(__FILE__), 'models', '*.rb')].each { |file| require file }
-require_relative 'helpers/data_mapper'
-require_relative 'helpers/warden'
-require 'pry
+# require_relative 'helpers/data_mapper'
+# require_relative 'helpers/warden'
+require 'pry'
 
 get '/' do
   "Hello Amber!"
