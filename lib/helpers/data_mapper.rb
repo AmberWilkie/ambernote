84390cc7ad4f7fdb6feff912cdec3802917f394f
Amber Wilkie <amber@amberwilkie.com>
2016-08-28 20:47:06 +0200

    Revert "Change ruby version, delete rakefile, move 'pry' gem"

--- lib/helpers/data_mapper.rb
+++
@@ -1,6 +0,0 @@
-require 'sinatra'
-require 'data_mapper'
-
-DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/ambernote_users_test")
-DataMapper.finalize
-DataMapper.auto_upgrade!
